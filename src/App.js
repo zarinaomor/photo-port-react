f43373b91d953fc838e503d6da86b1a104a4c84e
zarinaomor <zarinaomorova@yahoo.com>
2019-05-28 13:47:31 -0700

    create components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Register from './components/register/register'
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+ getPhotos = async () => {
+   
+  try {
+    const response = await fetch('http://localhost:8000/api/v1/photos', {
+      credentials: 'include'
+    });
+
+    if(response.ok){
+      const responseParsed = await response.json();
+      console.log(responseParsed)
+    }
+
+  } catch(err){
+    console.log(err)
+  }
+ }
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Register handleRegister={this.handleRegister} />
     </div>
-  );
+  )
+}
+
 }
 
 export default App;
