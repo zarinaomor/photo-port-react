cc64e346697c3cc8077cf90a31ad7bf34bdb3a7f
hrakt <hraktorres@gmail.com>
2019-05-28 17:18:22 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,72 +1,21 @@
 import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom';
-
-
-import Register from './components/register/register';
-import Navbar from './components/navbar/navbar';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import Login from './components/Login/Login';
+import Register from './components/register/register'
+import Navbar from './components/navbar/navbar'
 
-import * as routes from './constants/routes';
-
-class App extends Component {
-
-  componentDidMount (){
-    this.getPhotos()
-  }
-
-  handleRegister = async (data) => {
-    try {
-      const registerCall = await fetch('http://localhost:8000/users/', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Credentials': 'true'
-        }
-      })
-      const response = await registerCall.json()
-      console.log(response, 'from the flask server on localhost : 8000')
-
-    } catch(err){
-      console.log(err)
-    }
-  }
-    
-  getPhotos = async () => {
-    
-    try {
-      const response = await fetch('http://localhost:8000/photos', {
-        credentials: 'include',
-        method: "GET",
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-      });
-
-      if(response.ok){
-        const responseParsed = await response.json();
-        console.log(responseParsed)
-      }
-
-    } catch(err){
-      console.log(err)
-    }
-  }
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path={routes.REGISTER} render={()  => <Register handleRegister={this.handleRegister} />}/>
-
-        </Switch>
-      </div>
+        <main>
+            {/* <Switch> */}
+            <Navbar />
+                {/* <Route exact path="/login" component={ Login } /> */}
+                < Register  />
+            {/* </Switch> */}
+        </main>
     )
-  
-
-  };
 }
+
 
 export default App;
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -14,9 +14,31 @@ class Register extends Component {
             [e.target.name]: e.target.value
         })
     }
-    handleSubmit = (e) => {
+    handleSubmit = async(e) => {
         e.preventDefault()
-        this.props.handleRegister(this.state)
+        
+        try {
+            const loginResponse = await fetch('/', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': 'http://localhost:8000'
+                }
+            })
+
+            const parsedResponse = await loginResponse.json();
+            console.log(parsedResponse)
+            if(parsedResponse.success){
+                this.props.history.push('/photos')
+            }
+
+
+        } catch(err) {
+            console.log(err);
+            return err
+        }
+    
     }
     render(){
         return (
