bcf9ca55154ec661af9aaf53ffe4bda32d73cfdc
hrakt <hraktorres@gmail.com>
2019-05-28 19:16:49 -0700

    fix routing and add explore

---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Explore extends Component {
+    render(){
+        return (
+            <main>
+                <h1>Just some stuff</h1>
+            </main>
+        )
+    }
+}
+
+export default Explore;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 
-const App = () => {
+
+const Login = () => {
     return (
         <main>
-            <Switch>
-                <Route exact path="/login" component={ Login } />
-                <Route exact path="/register" component={ Register } />
-            </Switch>
+            <h1>Just some stuff</h1>
         </main>
     )
 }
