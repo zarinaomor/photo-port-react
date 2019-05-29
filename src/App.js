76b9619eb7c1eb9fa134beca86370178666682f3
zarinaomor <zarinaomorova@yahoo.com>
2019-05-28 17:16:49 -0700

    logn page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import Register from './components/register/register';
 
 const App = () => {
     return (
